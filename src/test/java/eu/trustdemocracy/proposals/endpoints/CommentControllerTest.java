337a263bbef614724430d8189f89aed706f3e5eb
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-05-10 03:29:38 +0200

    Extract common controller test functionality

---
+++ src/test/java/eu/trustdemocracy/proposals/endpoints/CommentControllerTest.java
@@ -0,0 +1,9 @@
+package eu.trustdemocracy.proposals.endpoints;
+
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(VertxUnitRunner.class)
+public class CommentControllerTest extends ControllerTest {
+
+}
